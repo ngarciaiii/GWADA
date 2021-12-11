d1c152766bc5b6d9524913818c32b9834d77c859
ngarciaiii <nicolas.garciaiii@gmail.com>
2021-12-10 23:21:31 -0600

    navbar ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { FrontimageComponent } from './frontimage/frontimage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
-    FrontimageComponent
+    FrontimageComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
